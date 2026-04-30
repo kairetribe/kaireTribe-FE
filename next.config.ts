7ace62e2aa4ba612160d24affb125123dc9d5b6e
ebuberoderick <ebuberoderick2@gmail.com>
2026-04-30 16:33:31 +0100

    fixing image deployment bug

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from "next";
 
+const supabaseHost = process.env.NEXT_PUBLIC_SUPABASE_URL
+  ? new URL(process.env.NEXT_PUBLIC_SUPABASE_URL).hostname
+  : undefined;
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["localhost", "127.0.0.1", "kairetribe.com", "www.kairetribe.com","images.unsplash.com"],
+    domains: [
+      "localhost",
+      "127.0.0.1",
+      "kairetribe.com",
+      "www.kairetribe.com",
+      "images.unsplash.com",
+      ...(supabaseHost ? [supabaseHost] : []),
+    ],
   },
 };
 
